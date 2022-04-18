177891ce857799ba23c503d23fad76dd39d91e23
ironman2057 <mattkaplan2057@gmail.com>
2022-04-18 01:04:42 -0400

    set rep data in SetsRepsFrag works

--- app/src/main/java/edu/fsu/cs/groupproject/graphs/Points.java
+++ app/src/main/java/edu/fsu/cs/groupproject/graphs/Points.java
@@ -2,19 +2,21 @@ package edu.fsu.cs.groupproject.graphs;
 
 public class Points
 {
-    int x;
-    int y;
-    int diff;
-    int day;
-    int lbs;
+    float x;
+    float y;
+    float diff_x;
+    float diff_y;
+    float day;
+    float lbs;
 
     //lbs should be value so can change to anything that will be on the y axis
-    Points(int d, int l, int x_val, int y_val, int change)
+    Points(float d, float l, float x_val, float y_val, float change_x,float change_y)
     {
         day = d;
         lbs = l;
         x = x_val;
         y = y_val;
-        diff = change;
+        diff_x = change_x;
+        diff_y = change_y;
     }
 }
